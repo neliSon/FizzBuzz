1f01c81a576162c326f4bfb188c7430ff88b177b
neliSon <[nelsonchow91.nc@gmail.com]>
2016-02-08 17:56:56 -0800

    Initial Commit

---
+++ FizzBizz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FizzBizz
+//
+//  Created by Nelson Chow on 2016-02-08.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FizzBizz/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  FizzBizz
+//
+//  Created by Nelson Chow on 2016-02-08.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
